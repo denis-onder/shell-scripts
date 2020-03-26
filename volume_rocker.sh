db4e1ed8b5b2b16f63a24e55e9ea5765e4a2e817
Denis Onder <denis_onder@protonmail.com>
2020-03-26 22:16:30 +0100

    Volume min/max

--- volume_rocker.sh
+++ volume_rocker.sh
@@ -6,7 +6,13 @@ then
 elif [ $1 = "down" ]
 then
   mpc volume -10
+elif [ $1 = "min" ]
+then
+  mpc volume 0
+elif [ $1 = "max" ]
+then
+  mpc volume 100
 else
   echo "Incorrect argument provided."
-  echo "Usage: volume_rocker <up/down>"
+  echo "Usage: volume_rocker <up/down/min/max>"
 fi
