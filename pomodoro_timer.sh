d382a3213dbf9f3967eced3217e321735aa3109f
Denis Onder <denis_onder@protonmail.com>
2020-04-10 12:03:49 +0200

    Seems like this is working

--- pomodoro_timer.sh
+++ pomodoro_timer.sh
@@ -1,6 +1,19 @@
-#!/bin/sh
-RESULT=`pgrep code`
+#!/bin/bash
+CODE=$(ps -A | grep code | awk '{ print $1 }')
+TIMER=$(ps -A | grep pomodoro-timer | awk '{ print $1 }')
 
-if [ "${RESULT:-null}" != null ]; then
-  pomodoro-timer start
+handle_start() {
+    if [ -z "$TIMER" ]; then
+        echo "Start timer"
+        pomodoro-timer start &
+    fi
+}
+
+if [ -n "$CODE" ]; then
+    echo "Running"
+    handle_start
+else
+    echo "Not running"
+    pkill pomodoro-timer
+    notify-send Pomodoro timer stopped.
 fi
